177ba5c71173d878a36de826076dc8da9fc1d2e3
Michael Henkel <mhenkel@juniper.net>
2019-04-01 19:11:29 -0700

    first commit

---
+++ pkg/apis/zookeeper/v1alpha1/zookeeper_types.go
@@ -0,0 +1,54 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ZookeeperSpec defines the desired state of Zookeeper
+// +k8s:openapi-gen=true
+type ZookeeperSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+	Size int32 `json:"size"`
+        Registry string `json:"registry"`
+        Version string `json:"version"`
+        HostNetwork bool `json:"hostnetwork"`
+}
+
+// ZookeeperStatus defines the observed state of Zookeeper
+// +k8s:openapi-gen=true
+type ZookeeperStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+	Nodes []string `json:"nodes"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Zookeeper is the Schema for the zookeepers API
+// +k8s:openapi-gen=true
+type Zookeeper struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ZookeeperSpec   `json:"spec,omitempty"`
+	Status ZookeeperStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ZookeeperList contains a list of Zookeeper
+type ZookeeperList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Zookeeper `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Zookeeper{}, &ZookeeperList{})
+}
