3d1a0d53506c9c966e2424f66e8db286b250ac87
Michael Henkel <mhenkel@juniper.net>
2019-05-15 14:29:39 +0200

    fix

--- deploy/create_manifest.sh
+++ deploy/create_manifest.sh
@@ -12,8 +12,10 @@ cat crds/tf_v1alpha1_tungstenfabricmanager_crd.yaml >> 1-create-operator.yaml
 echo "---" >> 1-create-operator.yaml
 cat operator.yaml >> 1-create-operator.yaml
 
-echo "---" > 2-start-operator.yaml
-cat crds/tf_v1alpha1_tungstenfabricmanager_cr.yaml >> 2-start-operator.yaml
+echo "---" > 2-start-operator-1node.yaml
+cat crds/tf_v1alpha1_tungstenfabricmanager_cr.yaml >> 2-start-operator-1node.yaml
+echo "---" > 2-start-operator-3node.yaml
+sed 's/size: "1"/size: "3"/g' 2-start-operator-1node.yaml > 2-start-operator-3node.yaml 
 
 echo "---" > 3-create-resources.yaml
 for i in `ls crds/*cr.yaml |grep -v tf_v1alpha1_tungstenfabricmanager_cr.yaml`
